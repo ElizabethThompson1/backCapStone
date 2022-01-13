0a753f792499e1e71e42e7fb805c5ad0aa0ca03f
ElizabethThompson1 <usawsoccer11@yahoo.com>
2022-01-13 15:44:28 -0600

    created a route that allowed users to get a user and there follings anf friends

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -12,6 +12,16 @@ const userCtrl = {
             return res.status(500).json({msg: err.message})
         }
     },
+    getUser: async (req,res)=>{
+        try {
+            const user =  await Users.findOne({_id : req.params.id})
+            .select("-password").populate("friends following" , "-password") 
+            if(!user) return res.status(400).json({msg: "No user Exists"})
+            res.json({user})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
     
     
 }
